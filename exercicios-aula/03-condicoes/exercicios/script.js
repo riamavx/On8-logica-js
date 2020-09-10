be31d8dd61d5d64d5bc0aeec5e21b39cf858e781
Maria Souza <messenger.mariah2017@gmail.com>
2020-09-10 16:24:26 -0300

    resolvendo exercicios

--- exercicios-aula/03-condicoes/exercicios/script.js
+++ exercicios-aula/03-condicoes/exercicios/script.js
@@ -18,11 +18,62 @@
   2. Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente;
 
 */
+// const diaSemana = parseInt(prompt('Digite o dia da semana [1-7]'));
+
+// switch (diaSemana){
+
+//     case 1:
+//         alert(`Domingo`);
+//         break;
+
+//         case 2:
+//         alert(`Segunda`);
+//         break;
+
+//         case 3:
+//         alert(`Terça-feira`);
+//         break;
+
+//         case 4:
+//         alert(`Quarta-feira`);
+//         break;
+
+//         case 5:
+//         alert(`Quinta-feira`);
+//         break;
+
+//         case 6:
+//         alert(`Sexta-feira`);
+//         break;
+
+//         case 7:
+//         alert(`Sábado`);
+//         break;
+
+//         default:
+//         alert(`Dia de São Nunca`);
+// }
+
+// console.log(diaSemana)
 
 
 /* 
   3. Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se ambos os números forem iguais, mostre uma mensagem no console "Os números são iguais";
 */
+
+// let numeroA = parseInt(prompt('Digite um número qualquer'));
+// let numeroB = parseInt(prompt('Digite um número qualquer'));
+
+// if(numeroA > numeroB){
+//   alert("O número A é maior ")
+// } else if( numeroA == numeroB){
+//   alert("Vixe, esses números são iguais")
+// }else{
+//   alert(" O número B é maior")
+// }
+  
+
+
 
 
 /*
@@ -37,3 +88,20 @@
   Alunos de recuperação devem ver a mensagem: Você está de recuperação!
   Alunos reprovados devem ver a mensagem: Que pena, você foi reprovado.
 */
+
+
+let primeiraNota = Number( prompt (`Digite sua primeira nota`));
+let segundaNota = Number( prompt (`Digite sua segunda nota`));
+let terceiraNota = Number( prompt (`Digite sua terceira nota`));
+
+
+const somaNotas = (primeiraNota + segundaNota + terceiraNota) /3
+
+if(somaNotas >= 7){
+  alert("Aprovado")
+}else if(somaNotas == 5 || somaNotas < 7 ){
+  alert("Recuperação")
+}else{ somaNotas < 5 
+  alert("Que pena, você foi reprovado.")
+
+}
